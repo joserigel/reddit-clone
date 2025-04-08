c278b23d79a6751d8321567a4a87b36ae43db237
joserigel <joserigel18@gmail.com>
2025-04-08 08:17:23 +0200

    added sidenav animation

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,11 +5,13 @@ interface ButtonProps {
   children?: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ noHover, children, className, style }: ButtonProps) {
+export default function Button({ noHover, children, className, style, onClick }: ButtonProps) {
   return (
     <button style={style}
+      onClick={onClick}
       className={`${!noHover && "hover:bg-gray-800"} 
       p-2 duration-300 flex justify-center 
       items-center h-10 rounded-full cursor-pointer
--- frontend/src/components/UserLayout.tsx
+++ frontend/src/components/UserLayout.tsx
@@ -1,6 +1,10 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
+
 import { FaBars, FaRegCommentDots, FaRegUserCircle } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
+import { BsChevronCompactLeft } from "react-icons/bs";
+
 import Button from "./Button";
 import Image from "next/image";
 
@@ -11,12 +15,15 @@ interface UserLayoutProps {
 }
 
 export default function UserLayout({ children }: UserLayoutProps) {
+  const [navShow, setNavShow] = useState<boolean>(false);
+
   return (<>
     <header className="flex flex-row justify-between 
       items-center p-1 fixed w-full bg-background h-18
       border-solid border-b-1 border-foreground z-50">
       <div className="flex flex-row">
-        <Button className="md:hidden">
+        <Button onClick={() => setNavShow(x => !x)}
+          className="md:hidden">
           <FaBars className="h-7 w-7"/>
         </Button>
         <Button noHover className="flex flex-row gap-2">
@@ -40,15 +47,20 @@ export default function UserLayout({ children }: UserLayoutProps) {
         </Button>
       </div>
     </header>
-    <aside className="border-1 md:min-w-10 pt-18 border-solid border-foreground h-full fixed">
-      <Button
-        className="absolute hidden md:block bg-background 
+    <aside
+      className={`
+      border-r-1 pt-18 border-solid border-foreground 
+      h-full fixed z-40 bg-background duration-500 md:w-96 w-full
+      ${navShow ? "translate-x-0" : "md:[transform:translateX(calc(-100%+3em))] [transform:translateX(-100%)]"}`}>
+      <Button onClick={() => setNavShow((x) => (!x))}
+        className="absolute hidden md:block bg-background
         border-1 border-solid border-foreground flex items-center justify-center
         top-30 right-0 translate-x-1/2">
-        <FaBars className="h-5 w-5"/>
+        { !navShow && <FaBars className="h-5 w-5"/> }
+        { navShow && <BsChevronCompactLeft className="h-5 w-5"/>}
       </Button>
     </aside>
-    <main className="pt-25   pl-15 relative">
+    <main className="pt-25 pl-15 relative z-10">
       {children}
     </main>
   </>)
